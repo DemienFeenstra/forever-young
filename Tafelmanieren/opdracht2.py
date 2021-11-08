c947c0ec1105291ca4b99b6eab9de4be9f4c9bfb
DemiënFeenstra <99064788@mydavinci.nl>
2021-11-08 11:30:49 +0100

    tafelmanieren af

---
+++ Tafelmanieren/opdracht2.py
@@ -0,0 +1,4 @@
+for i in range(30, 0, -1):
+    print(i)
+    import time
+    time.sleep(1)
---
+++ Tafelmanieren/opdracht3.py
@@ -0,0 +1,5 @@
+for i in range(1, 13):
+    print(i, "AM")
+
+for i in range(13, 25):
+    print(i, "PM")
