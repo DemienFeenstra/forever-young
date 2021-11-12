810237770d70e52042c7cbe88ce23efa76fd7c9d
DemiënFeenstra <99064788@mydavinci.nl>
2021-11-12 09:37:18 +0100

    opdracht 5 niet beschikbaar

--- robotarm-python-2021-main/opdracht5.py
+++ robotarm-python-2021-main/opdracht5.py
@@ -1,10 +1,10 @@
 from RobotArm import RobotArm
 
-robotArm = RobotArm('exercise 1')
+robotArm = RobotArm('exercise 5')
 
 # Jouw python instructies zet je vanaf hier:
 
-
+# opdracht 5 staat niet in de readme.md
 
 
 
